7fec5aade02af52ca50cf0a7724fe3ee70455c4f
iml6yu <iml6yu@gmail.com>
2020-05-08 15:48:33 +0800

    合并TEST到DEV  手工

---
+++ MicroFeel.YongYou.Models/Models/ScmItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MicroFeel.YongYou.Models.Models
+{
+    public partial class ScmItem
+    {
+        public string CInvCode { get; set; }
+        public string CFree1 { get; set; }
+        public string CFree2 { get; set; }
+        public string CFree3 { get; set; }
+        public string CFree4 { get; set; }
+        public string CFree5 { get; set; }
+        public string CFree6 { get; set; }
+        public string CFree7 { get; set; }
+        public string CFree8 { get; set; }
+        public string CFree9 { get; set; }
+        public string CFree10 { get; set; }
+        public byte[] Ufts { get; set; }
+        public int Id { get; set; }
+        public int? PartId { get; set; }
+    }
+}
